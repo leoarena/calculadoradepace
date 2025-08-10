37f163c17a54e61d13d170f24be9ed16d0a056ab
Leonardo Arena <leonardoarena7@gmail.com>
2025-08-10 14:59:47 -0300

    separa funções auxiliares em arquivo separado

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@
 
 import { useState, useRef } from "react";
 import styles from "./page.module.css";
+import {
+  formatarSegundosParaPace,
+  formatarSegundosParaTempo,
+  limitarMinutoSegundo,
+  paceParaSegundos,
+  tempoInputParaSegundos,
+} from "./utils/formatters";
 
 export default function Home() {
   const [calculo, setCalculo] = useState<"pace" | "tempo">("pace");
@@ -52,11 +59,6 @@ export default function Home() {
     ajustarFoco();
   };
 
-  const limitarMinutoSegundo = (valor: string): string => {
-    if (parseInt(valor) > 59) return "59";
-    else return valor;
-  };
-
   const formatarTempoInput = (valor: string): string => {
     const valorSanitizado = valor.replace(/\D/g, "").slice(0, 6);
 
@@ -92,18 +94,6 @@ export default function Home() {
     setVelocidadeResultado("");
   };
 
-  const tempoInputParaSegundos = (tempo: string): number => {
-    const [horas, minutos, segundos] = tempo.split(":").map(Number);
-    const totalSegundos = horas * 3600 + minutos * 60 + segundos;
-    return totalSegundos;
-  };
-
-  const formatarSegundosParaPace = (segundosPace: number): string => {
-    const minutos = Math.floor(segundosPace / 60);
-    const segundos = Math.round(segundosPace % 60);
-    return `${minutos}:${segundos.toString().padStart(2, "0")} min/km`;
-  };
-
   const ajustarFoco = () => {
     distanciaInputRef.current?.blur();
     tempoInputRef.current?.blur();
@@ -120,23 +110,6 @@ export default function Home() {
     setPaceInput(e.target.value);
     setTempoResultado("");
     setVelocidadeResultado("");
-  };
-
-  const formatarSegundosParaTempo = (totalSegundos: number): string => {
-    const horas = Math.floor(totalSegundos / 3600);
-    const minutos = Math.floor((totalSegundos % 3600) / 60);
-    const segundos = Math.round(totalSegundos % 60);
-    return `${horas.toString().padStart(2, "0")}:${minutos
-      .toString()
-      .padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
-  };
-
-  const paceParaSegundos = (pace: string): number => {
-    const match = pace.match(/(\d+):(\d+)/);
-    if (!match) return 0;
-    const minutos = parseInt(match[1]);
-    const segundos = parseInt(match[2]);
-    return minutos * 60 + segundos;
   };
 
   const calcularTempo = () => {
