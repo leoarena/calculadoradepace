d0ccae6582155a238826c43e10c60058f731bce8
Leonardo Arena <leonardoarena7@gmail.com>
2025-09-26 17:04:44 -0300

    refatora funções auxiliares para formatar segundos

--- app/utils/formatters.ts
+++ app/utils/formatters.ts
@@ -9,31 +9,37 @@ const tempoParaSegundos = (tempo: string): number => {
   return totalSegundos;
 };
 
-const formatarSegundosParaPace = (segundosPace: number): string => {
-  const minutos = Math.floor(segundosPace / 60);
-  const segundos = Math.round(segundosPace % 60);
-  return `${minutos}:${segundos.toString().padStart(2, "0")} min/km`;
-};
-
-const formatarSegundosParaTempo = (totalSegundos: number): string => {
-  const horas = Math.floor(totalSegundos / 3600);
-  const minutos = Math.floor((totalSegundos % 3600) / 60);
-  const segundos = Math.round(totalSegundos % 60);
-  return `${horas.toString().padStart(2, "0")}:${minutos
-    .toString()
-    .padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
-};
-
 const paceParaSegundos = (pace: string): number => {
   const [minutos, segundos] = pace.split(":").map(Number);
   const totalSegundos = minutos * 60 + segundos;
   return totalSegundos;
 };
 
+const formatarSegundosParaPace = (paceEmSegundos: number): string => {
+  const minutos = Math.floor(paceEmSegundos / 60);
+  const segundos = Math.round(paceEmSegundos % 60)
+    .toString()
+    .padStart(2, "0");
+  return `${minutos}:${segundos} min/km`;
+};
+
+const formatarSegundosParaTempo = (totalSegundos: number): string => {
+  const horas = Math.floor(totalSegundos / 3600)
+    .toString()
+    .padStart(2, "0");
+  const minutos = Math.floor((totalSegundos % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const segundos = Math.round(totalSegundos % 60)
+    .toString()
+    .padStart(2, "0");
+  return `${horas}:${minutos}:${segundos}`;
+};
+
 export {
   limitarMinutoSegundo,
   tempoParaSegundos,
+  paceParaSegundos,
   formatarSegundosParaPace,
   formatarSegundosParaTempo,
-  paceParaSegundos,
 };
